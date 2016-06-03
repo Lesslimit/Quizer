0cb9f14a494ea89c59fc0500c481788380f9b06c
panromanpan <V@V-PC>
2016-06-03 14:41:37 +0300

    Implemented domain objects

---
+++ src/Shared/Quizer.Domain/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quizer.Domain
+{
+    public class Test
+    {
+
+        [Required]
+        public int Id { get; set; }
+
+        public IList<Question> Questions { get; set; }
+
+        [Range(0, 100)]
+        public int Result { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ src/Shared/Quizer.Domain/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace Quizer.Domain
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public MailAddress Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int StudentsBookNumber { get; set; }
+
+        [Required]
+        public string Group { get; set; }
+
+        [Range(0, 100)]
+        public int Mark { get; set; }
+    }
+}
