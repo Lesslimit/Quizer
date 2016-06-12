467cf55aad93103175997d5f8a1b81779485451a
panromanpan <V@V-PC>
2016-06-12 16:29:32 +0300

    Implemented data access layer

---
+++ src/Web/Quizer.Website/src/dataService/dataService.ts
@@ -0,0 +1,55 @@
+﻿import {HttpClient} from 'aurelia-fetch-client';
+import {autoinject} from 'aurelia-framework';
+
+@autoinject()
+export class DataService {
+    httpClient: HttpClient;
+
+    constructor(httpClient: HttpClient) {
+          this.httpClient = httpClient;
+     }
+
+     configure(baseUrl) {
+         this.httpClient.configure(config => {
+             config
+                 .withBaseUrl(baseUrl)
+                 .withDefaults({
+                     credentials: 'same-origin',
+                     headers: {
+                         'Accept': 'application/json',
+                         'X-Requested-With': 'Fetch'
+                     }
+                 })
+                 .withInterceptor({
+                     request(request) {
+                         console.log(`Requesting ${request.method} ${request.url}`);
+                         return request;
+                     },
+                     response(response) {
+                         console.log(`Received ${response.status} ${response.url}`);
+                         return response;
+                     }
+                 });
+         });
+     }
+
+     get students() {
+        const ds = this;
+
+        return {
+            getAll() {
+                ds.httpClient.fetch('students/getAll').then(data => { });
+            }
+        }
+    }
+
+     get tests() {
+        const ds = this;
+
+        return {
+            getAll() {
+                ds.httpClient.fetch('tests/getAll').then(data => { });
+            }
+        }
+    }
+}
